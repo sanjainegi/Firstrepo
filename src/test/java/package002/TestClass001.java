4f9d76f08d47fe5c820c25c476654a5e04dec32a
sanjai <sanjai759@gmail.com>
2020-02-08 15:22:35 +0530

    conflict case

--- src/test/java/package002/TestClass001.java
+++ src/test/java/package002/TestClass001.java
@@ -23,8 +23,7 @@ public class TestClass001 {
 	public void Method3()
 	{
 		System.out.println("method3 printing its value");
-		System.out.println("let add method 3 new entry");
-		System.out.println("let local add method 3 new entry");
+		System.out.println("checking conflict changes");
 	}
 
 }
